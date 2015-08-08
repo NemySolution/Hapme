2f251137bd90a7587db2036fd05fad3aafd69e77
yeekeng <yksim90@hotmail.com>
2015-08-08 15:39:30 +0800

    updated create activity

--- mobile/src/main/java/sg/nemysolutions/hapme/CreateActivity.java
+++ mobile/src/main/java/sg/nemysolutions/hapme/CreateActivity.java
@@ -7,8 +7,12 @@ package sg.nemysolutions.hapme;
 //Key person: Yeekeng and Ming Sheng
 /************************************************/
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateActivity extends AppCompatActivity {
 
@@ -16,6 +20,16 @@ public class CreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create);
+
+
+        Button bn_addMember = (Button) findViewById(R.id.bn_addMember);
+
+        bn_addMember.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 }

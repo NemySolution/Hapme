be8261214cab6b0d4fa938ba5de0145331b19b8a
Ming Sheng <mingsheng36@gmail.com>
2015-08-11 22:16:38 +0800

    Added back callSign

--- mobile/src/main/java/sg/nemysolutions/hapme/CreateActivity.java
+++ mobile/src/main/java/sg/nemysolutions/hapme/CreateActivity.java
@@ -80,6 +80,7 @@ public class CreateActivity extends AppCompatActivity {
                     operation = new ParseObject("Operation");
                     operation.put("deviceId", deviceId);
                     operation.put("opsName", et_opsName.getText().toString());
+                    operation.put("callSign", et_callSign.getText().toString());
                     operation.put("secretKey", et_secretKey.getText().toString());
                     operation.saveInBackground(new SaveCallback() {
                         @Override

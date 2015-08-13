7467940d850ff8f111edbd467897327316d5c1c9
nvcwk <nicholasvinchanwk@msn.com>
2015-08-13 19:21:34 +0800

    Toast Message notifications for testing.

--- mobile/src/main/java/sg/nemysolutions/hapme/CustomReceiver.java
+++ mobile/src/main/java/sg/nemysolutions/hapme/CustomReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -14,7 +15,9 @@ public class CustomReceiver extends BroadcastReceiver {
         try {
             JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
 
-            Log.e("hello", "Push received: " + json);
+            String message = json.getString("alert");
+
+            Toast.makeText(context, "Message: " + message + " RECEIVED!", Toast.LENGTH_SHORT).show();
 
         } catch (JSONException e) {
             Log.e("hello", "Push message json exception: " + e.getMessage());

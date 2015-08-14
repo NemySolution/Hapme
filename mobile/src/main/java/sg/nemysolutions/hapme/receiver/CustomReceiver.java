8533d90873aea026e8282a87215c0890e4e55486
yiikaii <esmondpeh@gmail.com>
2015-08-14 19:02:34 +0800

    Testing notifcation

--- mobile/src/main/java/sg/nemysolutions/hapme/receiver/CustomReceiver.java
+++ mobile/src/main/java/sg/nemysolutions/hapme/receiver/CustomReceiver.java
@@ -1,8 +1,12 @@
 package sg.nemysolutions.hapme.receiver;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -11,10 +15,13 @@ import com.parse.ParsePushBroadcastReceiver;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import sg.nemysolutions.hapme.R;
+import sg.nemysolutions.hapme.activity.OperationActivity;
+
 public class CustomReceiver extends ParsePushBroadcastReceiver {
     @Override
     protected void onPushReceive(Context context, Intent intent) {
-        super.onPushReceive(context, intent);
+        //super.onPushReceive(context, intent);
 
         try {
             JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
@@ -22,6 +29,33 @@ public class CustomReceiver extends ParsePushBroadcastReceiver {
             String message = json.getString("alert");
 
             Toast.makeText(context, "Message: " + message + " RECEIVED!", Toast.LENGTH_SHORT).show();
+
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+                            .setVibrate(new long[] { 100, 1000, 100, 2000, 1000 })
+                            .setContentTitle("ALERT!")
+                            .setContentText(message);
+            // Creates an explicit intent for an Activity in your app
+                        Intent resultIntent = new Intent(context, OperationActivity.class);
+
+            // The stack builder object will contain an artificial back stack for the
+            // started Activity.
+            // This ensures that navigating backward from the Activity leads out of
+            // your application to the Home screen.
+                        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+            // Adds the back stack for the Intent (but not the Intent itself)
+                        stackBuilder.addParentStack(OperationActivity.class);
+            // Adds the Intent that starts the Activity to the top of the stack
+                        stackBuilder.addNextIntent(resultIntent);
+                        PendingIntent resultPendingIntent =
+                                stackBuilder.getPendingIntent(
+                                        0,
+                                        PendingIntent.FLAG_UPDATE_CURRENT
+                                );
+                        mBuilder.setContentIntent(resultPendingIntent);
+                        NotificationManager mNotificationManager =
+                                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            // mId allows you to update the notification later on.
+            mNotificationManager.notify(100, mBuilder.build());
 
         } catch (JSONException e) {
             Log.e("hello", "Push message json exception: " + e.getMessage());

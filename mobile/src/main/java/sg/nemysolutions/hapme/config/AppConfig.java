3c3872f8323d7d9e35d09cfcd3e698042b666175
Esmond Peh <esmondpeh@gmail.com>
2015-08-14 14:19:55 +0800

    new config class and update javadocs

---
+++ mobile/src/main/java/sg/nemysolutions/hapme/config/AppConfig.java
@@ -0,0 +1,7 @@
+package sg.nemysolutions.hapme.config;
+
+public class AppConfig {
+
+    public static final String parseAppId = "0RgU1BAusmGZiIkOFDVucZcEtbCHPiJx479CDcKG";
+    public static final String parseClientKey = "GhyTkHlqG22YziVd7fbP8YNTYK6wbmrcwF99yM5G";
+}
--- mobile/src/main/java/sg/nemysolutions/hapme/utilities/ParseUtils.java
+++ mobile/src/main/java/sg/nemysolutions/hapme/utilities/ParseUtils.java
@@ -6,21 +6,35 @@ import android.content.Intent;
 import com.parse.Parse;
 import com.parse.ParseInstallation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import sg.nemysolutions.hapme.activity.OperationActivity;
+import sg.nemysolutions.hapme.config.AppConfig;
 
 public class ParseUtils {
+    /**
+     *
+     * @param context
+     */
     public static void registerParse(Context context) {
         // Enable Local Datastore.
         Parse.enableLocalDatastore(context);
-        Parse.initialize(context, "0RgU1BAusmGZiIkOFDVucZcEtbCHPiJx479CDcKG", "GhyTkHlqG22YziVd7fbP8YNTYK6wbmrcwF99yM5G");
+        Parse.initialize(context, AppConfig.parseAppId, AppConfig.parseClientKey);
         ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 
-    public List<String> getChannels() {
+    /**
+     *
+     * @return a list of channel if not empty else an empty list
+     */
+    public static List<String> getChannels() {
         // Check with Parse whether this user is in any channels
-        List<String> subscribedChannels = ParseInstallation.getCurrentInstallation().getList("channels");
+        List<String> subscribedChannels = new ArrayList<>();
+
+        if (!ParseInstallation.getCurrentInstallation().getList("channels").isEmpty()) {
+            subscribedChannels = ParseInstallation.getCurrentInstallation().getList("channels");
+        }
 
         return subscribedChannels;
     }

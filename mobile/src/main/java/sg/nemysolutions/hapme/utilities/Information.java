52fdb454f6c1bca0ed97a2bf260824ce42b9f700
yiikaii <esmondpeh@gmail.com>
2015-08-14 13:57:40 +0800

    Added Information class

---
+++ mobile/src/main/java/sg/nemysolutions/hapme/utilities/Information.java
@@ -0,0 +1,27 @@
+package sg.nemysolutions.hapme.utilities;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+public class Information {
+
+    /**
+     *
+     * @param context
+     * @return
+     */
+    public static String getVersion(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("Fetch Version Error", "Error:", e);
+
+            return "Fetch Version Error";
+        }
+    }
+}
